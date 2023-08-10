249652ec924a1d20a3e1c46968c09db240902cc2
Max-munene <munenemaxwell2@gmail.com>
2023-08-10 09:17:10 +0300

    @Output Interaction

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child-component',
-  template: `Say {{ childMessage }}`,
+  template: `Say {{ childMessage }}
+    <button (click)="sendMessage()">Say Hi</button>`,
   styleUrls: ['./child-component.component.css'],
 })
 export class ChildComponentComponent {
   @Input() childMessage!: string;
   constructor() {}
+  @Output() messageEvent = new EventEmitter<string>();
+
+  sendMessage() {
+    this.messageEvent.emit('Hello August!');
+  }
 }
--- src/app/parent-component/parent-component.component.ts
+++ src/app/parent-component/parent-component.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-parent-component',
-  template: `<app-child-component
-    [childMessage]="parentMessage"
-    ]
-  ></app-child-component>`,
+  template: `<h1>{{ message }}</h1>
+    <app-child-component
+      (messageEvent)="message = $event"
+      [childMessage]="parentMessage"
+    ></app-child-component>`,
   styleUrls: ['./parent-component.component.css'],
 })
 export class ParentComponentComponent {
   parentMessage = 'Hellooo family';
   constructor() {}
+  message = '';
 }
