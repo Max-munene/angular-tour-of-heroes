cbb0e0529bdceade4e77e96f492c707fff02748d
Max-munene <munenemaxwell2@gmail.com>
2023-08-15 09:38:22 +0300

    comment

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -1,17 +1,21 @@
+// Import necessary Angular modules and decorators
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-child-component',
+  // Component metadata: selector, template, and styles
+  selector: 'app-child-component', // Selector to use this component in templates
   template: `Say {{ childMessage }}
-    <button (click)="sendMessage()">Say Hi</button>`,
-  styleUrls: ['./child-component.component.css'],
+    <button (click)="sendMessage()">Say Hi</button>`, // Component template with data binding
+  styleUrls: ['./child-component.component.css'], // Component's style file
 })
 export class ChildComponentComponent {
-  @Input() childMessage!: string;
-  constructor() {}
-  @Output() messageEvent = new EventEmitter<string>();
+  @Input() childMessage?: string; // Input property to receive data from parent component
+  constructor() {} // Constructor for the component, runs when the component is created
 
+  @Output() messageEvent = new EventEmitter<string>(); // Output property for sending data to parent component
+
+  // Method to send a message to the parent component
   sendMessage() {
-    this.messageEvent.emit('Hello August!');
+    this.messageEvent.emit('Hello August!'); // Emitting the event with a message
   }
 }
--- src/app/parent-component/parent-component.component.ts
+++ src/app/parent-component/parent-component.component.ts
@@ -1,16 +1,19 @@
+// Import necessary Angular module
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-parent-component',
+  // Component metadata: selector, template, and styles
+  selector: 'app-parent-component', // Selector to use this component in templates
   template: `<h1>{{ message }}</h1>
+    <!-- Embedding the child component with inputs and outputs -->
     <app-child-component
       (messageEvent)="message = $event"
       [childMessage]="parentMessage"
     ></app-child-component>`,
-  styleUrls: ['./parent-component.component.css'],
+  styleUrls: ['./parent-component.component.css'], // Component's style file
 })
 export class ParentComponentComponent {
-  parentMessage = 'Hellooo family';
-  constructor() {}
-  message = '';
+  parentMessage = 'Hellooo family'; // Property to be passed to child component
+  constructor() {} // Constructor for the component, runs when the component is created
+  message = ''; // Property to store the message received from the child component
 }
